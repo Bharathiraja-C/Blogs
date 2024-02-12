3abf9d0d0c4c7ee90f5ad428086c40ccb5e02374
Abhishek kumar <95436751+Abhishek-1087@users.noreply.github.com>
2024-02-13 01:16:34 +0530

    Created Blog.js

---
+++ backend/models/Blog.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    content: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
