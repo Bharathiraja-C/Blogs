b1d6fb74f955fa05d51c321185dbe7fcc4678f08
Ashmitha Laxmi <ashmithalaxmi@jmangroup.com>
2024-02-12 15:32:03 +0530

    Added Blog Reading page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import UserProfile from './Pages/UserProfile/UserProfile';
 import UserAuth from './Pages/UserAuth/UserAuth';
+import BlogPostPage from './Pages/BlogRead/BlogRead';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/User/:id' element={<UserProfile />} />
         <Route path='/Auth' element={<UserAuth/>}/>
+        <Route path='/Blog' element={<BlogPostPage/>}/>
       </Routes>
     </BrowserRouter>
   );
