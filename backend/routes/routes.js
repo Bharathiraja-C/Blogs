d50e077b6ee1b6bd9e7284ac860a9d110ad75412
Abhishek kumar <95436751+Abhishek-1087@users.noreply.github.com>
2024-02-13 01:14:49 +0530

    Updated routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,12 +1,10 @@
-const express = require("express")
-const router = express.Router()
-const authController = require('../controllers/Auth')
-const blogController = require('../controllers/Blog')
+const express = require("express");
+const router = express.Router();
+const authController = require('../controllers/Auth');
+const blogController = require('../controllers/Blog');
 
-// contains all the .get, .post, .put, ... etc routing parts
+router.post('/signUp', authController.signUp);
+router.get('/getAllBlogs', blogController.getAllBlogs);
+router.post('/newBlog', blogController.createBlog); // Add this new route
 
-router.post('/signUp', authController.signUp) 
-
-router.get('/getAllBlogs', blogController.getAllBlogs)
-
-module.exports = router
+module.exports = router;
