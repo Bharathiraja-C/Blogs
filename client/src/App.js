e2804005c2cc01ea3c98a30ce512ce644ffd4c14
manii7663 <manishabhandari119@gmail.com>
2024-02-12 15:27:01 +0530

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import UserProfile from './Pages/UserProfile/UserProfile';
+import Home from './Components/UserProfile/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/User/:id' element={<UserProfile />} />
       </Routes>
     </BrowserRouter>
