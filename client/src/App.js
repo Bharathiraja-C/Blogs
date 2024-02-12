8b1b0b224919c545922460b0445d93d2e89cf3b0
Ashmitha Laxmi <ashmithalaxmi@jmangroup.com>
2024-02-12 18:18:49 +0530

    Added the hero div

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import UserAuth from './Pages/UserAuth/UserAuth';
 import Home from './Components/UserProfile/Home';
 import BlogPostPage from './Pages/BlogRead/BlogRead';
 import NewBlog from './Pages/NewBlog/NewBlog';
+import HeroDiv from './Components/UserProfile/HeroDiv';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/Auth' element={<UserAuth />} />
         <Route path='/blog/:id' element={<BlogPostPage />} />
         <Route path='/newBlog' element={<NewBlog/>}/> 
+        <Route path='/hero' element={<HeroDiv/>}/> 
 
       </Routes>
     </BrowserRouter>
